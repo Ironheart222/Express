da47f3d9d7a6344b16af911d67b570cc4dfd707f
Ironheart222 <kanopatel222.kp@gmail.com>
2021-02-25 12:34:47 +0530

    changes

--- server.js
+++ server.js
@@ -8,14 +8,17 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
 
 app.use(productRouter);
 
 app.use(mainRouter);
 
+app.use((req, res, next) => {
 
-
-
+    return res.json({ message: 'page not found'})
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`);
