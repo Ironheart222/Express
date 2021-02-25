dfe15f2719ab06aa3094fa24816292b2947cc5f6
Ironheart222 <kanopatel222.kp@gmail.com>
2021-02-25 18:12:40 +0530

    changes

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const ErrorHandler = require('../errors/ErrorHandler');
 let products = require('../productData');
+const apiKeyMiddleware = require('../middlewares/apiKey');
 
 router.get('/products', (req, res) => {
     res.render('products', {
@@ -7,31 +9,36 @@ router.get('/products', (req, res) => {
     });
 });
 
-router.get('/api/products', (req,res,) => {
-
+router.get('/api/products', (req, res) => {
     res.json(products);
 });
 
-router.post('/api/products', (req, res,) => {
-    const { name,price } = req.body;
+router.post('/api/products', apiKeyMiddleware, (req, res, next) => {
+    // try {
+    //     console.log(city);
+    // } catch(err) {
+    //     next(ErrorHandler.serverError(err.message));
+    // }
 
-    if(!name || !price) {
-        return res.status(422).json({error: 'All fields are required.'});
+    const { name, price } = req.body;
 
+    if (!name || !price) {
+        next(ErrorHandler.validationError('Name and price fields are required!'));
+        // throw new Error('All fields are required!');
+        // return res.status(422).json({ error: 'All fields are required.'});
     }
     const product = {
         name,
         price,
         id: new Date().getTime().toString()
     }
-
-        products.push(product)
-         res.json(product );
+    products.push(product);
+    res.json(product);
 });
 
-router.delete('/api/products/:productId',(req,res) =>{
+router.delete('/api/products/:productId', (req, res) => {
     products = products.filter((product) => req.params.productId !== product.id);
-    res.json({ status: 'ok' });
+    res.json({ status: 'OK' });
 });
 
 module.exports = router;
